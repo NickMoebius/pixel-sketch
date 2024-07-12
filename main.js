fc698deb55d130015d9c2d56176985d77545878e
Nick Moebius <nickmoebiusec@gmail.com>
2024-07-12 19:55:05 +0300

    Added drawing function and 'Clear' button

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("#canvas");
+const body = document.querySelector("body");
 
 
 function pixelGenerator() {
@@ -13,8 +14,65 @@ function pixelGenerator() {
     }
 }
 
+function pixelColoring() {
+
+    let isMouseDown;    
+    
+    canvas.addEventListener('mousedown', (event) => {
+        isMouseDown = true;
+        let target = event.target;
+        
+        if (isMouseDown && target.id != 'canvas') {
+            console.log(target.id);
+            target.style.backgroundColor = "rgba(37, 37, 37, 100%)";
+            target.style.border = "1px solid white";
+        }
+    });
+
+    canvas.addEventListener('mouseup', (event) => {
+        isMouseDown = false;
+    });
+
+    canvas.addEventListener('mouseover', (event) => {
+        let target = event.target;
+
+        console.log(event);
+        if (isMouseDown || target.style.backgroundColor === "rgba(37, 37, 37)") {            
+            target.style.backgroundColor = "rgba(37, 37, 37, 100%)";
+            target.style.border = "1px solid white";
+        } else {
+            target.style.border = "1px solid black";
+        } 
+    });
+
+    body.addEventListener('mouseout', (event) => {
+        let target = event.target; 
+        
+        if (target.className === "pixel") {
+            target.style.border = "none"; 
+        } else {
+            isMouseDown = false;
+        }
+               
+    });   
+}
+
+function clearButton() {
+    const btn = document.querySelector("#clear-btn");
+    let pixels = document.querySelectorAll(".pixel");
+
+    btn.addEventListener('click', (event) => {
+        for (let pixel of pixels) {
+            pixel.style.backgroundColor = "rgba(37, 37, 37, 5%)";
+        }
+        
+    });      
+}
+
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    pixelGenerator(); 
+    pixelGenerator(),
+    pixelColoring(),
+    clearButton()
 });
